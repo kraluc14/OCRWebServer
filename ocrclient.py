567fe547a2eaf5e4a7827db0df866d9abec1b859
lukas <lukas@localhost.localdomain>
2019-04-24 15:40:11 +0200

    Added gitignore and removed more unused files

--- ocrclient.py
+++ ocrclient.py
@@ -3,7 +3,7 @@ import json
 import os
 
 
-url = 'http://127.0.0.1:5000/ocrjson'
+url = 'http://192.168.178.68:5000/ocrjson'
 payload = {"minConfidence": 0.5, "width": 320, "height": 320, "padding": 0, "language":"eng", "oem":1, "psm": 7}
 file='/home/lukas/Desktop/example_03.jpg'
 files = {

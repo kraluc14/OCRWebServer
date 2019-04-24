0b71e45b3c3e4bc03487ab9f0b06533a35706780
Lukas Kranz <39590080+kraluc14@users.noreply.github.com>
2019-04-24 16:27:07 +0200

    added requirements as sh file

---
+++ requirements.sh
@@ -0,0 +1,13 @@
+python3 -m pip install flask
+
+python3 -m pip install imutils
+
+sudo apt-get install tesseract-ocr
+
+python3 -m pip install pytesseract
+
+python3 -m pip install opencv-python
+
+python3 -m pip install cv2
+
+sudo apt-get install python3-opencv
